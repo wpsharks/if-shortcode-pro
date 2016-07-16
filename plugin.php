5c592a1e20860d14ddfa8cacfef4a88fd58b298f
jaswsinc <jaswsinc@wsharks.com>
2016-07-15 18:47:47 -0800

    Remove closing `/` bracket from description.

--- plugin.php
+++ plugin.php
@@ -15,7 +15,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
  * Plugin URI: https://wpsharks.com/product/if-shortcode
- * Description: [if /] shortcode for WordPress.
+ * Description: [if] shortcode for WordPress.
  */
 // PHP v5.2 compatible.
 
