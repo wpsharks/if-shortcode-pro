5b85cb1631ff22165aeb4dcf45ea6f3f67bf656e
jaswrks <jas@src.works>
2017-03-08 04:50:35 -0900

    Updating build.

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  *
  * @wp-plugin
  *
- * Version: 170308.49719
+ * Version: 170308.49783
  * Text Domain: if-shortcode
  * Plugin Name: [if] Shortcode Pro
  *
--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -42,7 +42,7 @@ class App extends SCoreClasses\App
      *
      * @type string Version.
      */
-    const VERSION = '170308.49719'; //v//
+    const VERSION = '170308.49783'; //v//
 
     /**
      * Constructor.
